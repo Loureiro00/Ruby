f1260afb7feb5bc78e9aa3cad74f6c2ee07482bd
Loureiro00 <pedrohstloureiro@gmail.com>
2024-03-04 09:53:18 -0300

    Desafio

---
+++ aula4.rb
@@ -0,0 +1,12 @@
+# entrada e saída de dados
+# 1 ) saída de dados na tela ---> aparecer um comando na tela
+# usuario digite seu nome
+print "Digite seu nome : "
+
+# 2 ) quero que meu programa permita que o usuario preencha oq foi pedido gets.chomp
+nome = gets.chomp
+
+print "Digite seu sobrenome : "
+sobrenome = gets.chomp
+#3) saida final
+puts " Oi #{nome} #{sobrenome} ! "
---
+++ aula4Soma.rb
@@ -0,0 +1,11 @@
+# usuario digite 1 numero
+# pedir ao usuario
+#.to_i
+print "Digite um número inteiro: "
+num1= gets.chomp.to_i
+# usuario digite outro numero
+print "Digite um número inteiro: "
+num2= gets.chomp.to_i
+# some esses numeros do usuario
+soma = num1 + num2
+puts " O resultado da soma é de :  #{soma}"
